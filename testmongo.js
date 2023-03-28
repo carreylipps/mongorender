e37774eb61b5965049992887ddb8515c3041f4b2
carreylipps <85637986+carreylipps@users.noreply.github.com>
2023-03-28 11:32:09 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -28,9 +28,9 @@ app.get('/say/:name', function(req, res) {
 
 
 // Route to access database:
-app.get('/api/mongo/:item', function(req, res) {
+app.get('/api/mongo/:itemID', function(req, res) {
 const client = new MongoClient(uri);
-const searchKey = "{ partID: '" + req.params.item + "' }";
+const searchKey = "{ itemID: '" + req.params.itemID + "' }";
 console.log("Looking for: " + searchKey);
 
 async function run() {
@@ -41,7 +41,7 @@ async function run() {
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
     // But we will use the parameter provided with the route
-    const query = { partID: req.params.item };
+    const query = { itemID: req.params.item };
 
     const part = await parts.findOne(query);
     console.log(part);
